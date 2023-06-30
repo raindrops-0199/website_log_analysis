5e74c50ea8a23242c9f4058592b73076ccc41b39
jason <lkarry0199@Gmail.com>
2023-06-30 16:02:41 +0800

    Finish Task3

---
+++ task3/IPCount/src/IPCount/ResInputFormat.java
@@ -0,0 +1,20 @@
+package IPCount;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import java.io.IOException;
+
+public class ResInputFormat extends FileInputFormat<LongWritable, Text> {
+
+    @Override
+    public RecordReader<LongWritable, Text> createRecordReader(InputSplit split, TaskAttemptContext context) throws IOException{
+        ResRecordReader reader = new ResRecordReader();
+        reader.initialize(split, context);
+        return reader;
+    }
+}
